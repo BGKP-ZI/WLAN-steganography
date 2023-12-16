81820b16bb32c3ca0b0b15ff92b617be480e9fc8
IvanKhrol <khrol_ivan@mail.ru>
2023-12-16 12:24:43 +0300

    add chats layout

--- Graphics/sources/chats_layout/chats_layout.hh
+++ Graphics/sources/chats_layout/chats_layout.hh
@@ -2,6 +2,11 @@
 #define __CHATS_LAYOUT_hh__
 
 #include <QWidget>
+#include <QPushButton>
+#include <QFileDialog>
+#include <QMessageBox>
+
+#include <WLAN.hh>
 
 namespace Ui {
 class Chats_layout;
@@ -14,8 +19,20 @@ public:
   explicit Chats_layout(QWidget *parent = nullptr);
   ~Chats_layout();
 
+  QPushButton* btn_receive;
+  QPushButton* btn_load_file;
+
+public slots:
+  void set_mac_addres(Network::MACAddress &mac);
+
 private:
   Ui::Chats_layout *ui;
+  Network::MACAddress MAC;
+  int receive_mod = 1;
+
+private slots:
+  void change_mode();
+  void load_file();
 };
 
 #endif // __CHATS_LAYOUT_hh__
