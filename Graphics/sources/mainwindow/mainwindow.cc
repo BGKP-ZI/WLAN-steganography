68f90928b9f4a8bcaa0793ea535c079b80ab84c3
IvanKhrol <khrol_ivan@mail.ru>
2023-12-16 05:35:33 +0300

    Fix

--- Graphics/sources/mainwindow/mainwindow.cc
+++ Graphics/sources/mainwindow/mainwindow.cc
@@ -32,7 +32,6 @@ void MainWindow::setup_MainWindow() {
   setWindowTitle(APP_NAME);
   setWindowFlags(Qt::Window);
   QString path_icon{":/klimanov.ico"};
-  qDebug() << 
   if(!QFile(":/klimanov.ico").exists()) {
     qWarning() << "Icon file not found";
   }
@@ -58,13 +57,13 @@ void MainWindow::setup_ToolBar() {
   connect(ui->toolBar, SIGNAL(customContextMenuRequested(QPoint)), this,
           SLOT(void_slot()));
 
-  // Show Chats_layout widget by CTRL+SHIFT+C
-  connect(ui->actionChats, SIGNAL(triggered()), this, SLOT(run_chats()));
-  ui->toolBar->addAction(ui->actionChats);
-
   // Show Newtwork_layout widget by CTRL+SHIFT+N
   connect(ui->actionNetwork, SIGNAL(triggered()), this, SLOT(run_network()));
   ui->toolBar->addAction(ui->actionNetwork);
+
+  // Show Chats_layout widget by CTRL+SHIFT+C
+  connect(ui->actionChats, SIGNAL(triggered()), this, SLOT(run_chats()));
+  ui->toolBar->addAction(ui->actionChats);
 
   // Show Settings_layout widget by CTRL+SHIFT+S
   connect(ui->actionSettings, SIGNAL(triggered()), this, SLOT(run_settings()));
