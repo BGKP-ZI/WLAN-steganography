a304461dccb7f809e827a8aeb9adf1e7351c0e8a
gubanovpm <petrgubanov37@gmail.com>
2023-12-16 06:11:00 +0300

    trying to work in one branch

--- Network/samples/wlan_sender/wlan_sender.cc
+++ Network/samples/wlan_sender/wlan_sender.cc
@@ -3,7 +3,7 @@
 int main(void) {
   Network::WLAN wlan("wlan0");
   wlan.ifconfig_info(std::cout);
-  Network::MACAddress addr("14:7d:da:86:09:4d", true);
+  Network::MACAddress addr("69:03:e8:60:a4:b0", true);
 	std::string message = "";
 	for (std::size_t i = 0; i < 1520; ++i) {
 		message += "a";
---
+++ Network/sources/RawData.cc
@@ -0,0 +1,17 @@
+#include <RawData.hh>
+
+namespace Network {
+
+// RawData::RawData(const std::string &data) {
+
+// }
+
+// uint32_t crc_calc(const std::string &data) {
+//     return 0;
+// }
+
+// void RawData::crc_calc(void) {
+//     return;
+// }
+
+}
